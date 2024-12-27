01ca5f400e78368666b8bd8820cd58daa945aa03
Justin Miller <justinmiller617@gmail.com>
2024-12-27 12:31:59 -0600

    Added custom CSS styles, updated site logo, and changed page title

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,30 +1,28 @@
 import { Link } from 'react-router-dom';
-import Logo from '../assets/images/NullLabsLogo.png';
+import Logo from '../assets/images/NullLabsLogo2.png';
+import './NavBar.css'
 
 const NavBar = () => {
     return (
         <div>
-           <header>
-                <div>
-                    <img src={Logo} alt="NullLabsLogo"/>
-                    <h1>Null Labs</h1>
+           <header className="header">
+                <div className="header__logo-title">
+                    <img src={Logo} alt="NullLabsLogo" className="header__logo"/>
+                    {/* <h1 className="header__title">Null Labs</h1> */}
                 </div>
-                <nav>
-                    <ul>
+                <nav className="header__nav">
+                    <ul className="header__menu">
                         <li>
-                            <Link to="/">Home</Link>
+                            <Link to="/" className="header__link">Home</Link>
                         </li>
                         <li>
-                            <Link to="/featuredgames">Featured Games</Link>
+                            <Link to="/featuredgames" className="header__link">Featured Games</Link>
                         </li>
                         <li>
-                            <Link to="/dealoftheweek">Deal of the Week!</Link>
+                            <Link to="/dealoftheweek" className="header__link">Deal of the Week!</Link>
                         </li>
                         <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/profile">Profile</Link>
+                            <Link to="/profile" className="header__link">Profile</Link>
                         </li>
                     </ul>
                 </nav>
